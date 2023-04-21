b4d2bd7033a93967282253ec6e44ab0c01575191
BradenKim22 <118400033+BradenKim22@users.noreply.github.com>
2023-04-20 18:13:28 -0600

    added all the prompts

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
 });
 
 function init() {
-    inquirer.createPromptModule({
+    inquirer.prompt({
         name: "begin",
         type: "list",
         message: "What action do you want to make?",
@@ -62,21 +62,156 @@ const viewDepartments = () => {
     let query = "SELECT * FROM department";
     connection.query(query, function (err, res) {
         if (err) throw err;
-        console.log(res);
+        console.table(res);
+        init();
     });
 };
+
 // view roles
+const viewRoles = () => {
+    let query = "SELECT role.title, role.salary, role.id, department.name AS department FROM role RIGHT JOIN department ON role.department_id = department.id";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        init();
+    });
+};
 
 // view employees
+const viewEmployees = () => {
+    let query = "SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS job, department.name AS department, role.salary, t4.first_name AS manager FROM role INNER JOIN employee ON role.id = employee.role_id INNER JOIN department ON department.id = role.department_id INNER JOIN employee t4 ON employee.manager_id = t4.id";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        init();
+    });
+};
 
 // add department
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            name: "departmentName",
+            type: "input",
+            message: "New Department Name?",
+        }
+    ]).then( function (answer) {
+        connection.query("INSERT INTO department SET ?",
+        {
+            name: answer.departmentName
+        }),        
+        init();
+    });
+};
 
 // add roles
+const addRole = () => {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw (err);
+        inquirer.prompt([
+            {
+                name: "roleName",
+                type: "input",
+                message: "New Role Name?",
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "Salary for the New Role?",
+            },
+            {
+                name: "departmentID",
+                type: "list",
+                choices: function () {
+                    return res.map((role) => ({ name: department.name, value: department.id }))
+                },
+                message: "Select the Department for the New Role.",
+            },
+        ]).then( function (answer) {
+            connection.query("INSERT INTO role SET ?",
+            {
+                title: answer.roleName,
+                salary: answer.salary,
+                department_id: answer.departmentID,
+            }),
+            init();
+        });
+    });
+};
 
 // add employees
+const addEmployee = () => {
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw (err);
+        inquirer.prompt([
+            {
+                name: "firstName",
+                type: "input",
+                message: "First Name?",
+            },
+            {
+                name: "lastName",
+                type: "input",
+                message: "Last Name?",
+            },
+            {
+                name: "roleID",
+                type: "list",
+                choices: function () {
+                    return res.map((role) => ({ name: role.title, value: role.id }))
+                },
+                message: "Select the Role",
+            },
+            {
+                name: "managerID",
+                type: "input",
+                message: "Manager ID?",
+            },
+        ]).then( function (answer) {
+            connection.query("INSERT INTO employee SET ?",
+            {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: answer.roleID,
+                manager_id: answer.managerID,
+            }),
+
+            init();
+
+        });
+    });
+};
 
 // update employee roles
-
+const updateEmployee = () => {
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw (err);
+        inquirer.prompt([
+            {
+                name: "employeeID",
+                type: "list",
+                choices: function () {
+                    return res.map((employee) => ({ name: employee.first_name, value: employee.id }))
+                },
+                message: "Employee ID?",
+            },
+            {
+                name: "updateEmployee",
+                type: "list",
+                choices: function () {
+                    return res.map((role) => ({ name: role.title, value: role.id }))
+                },
+                message: "Update the Employee Role",
+            },
+        ]).then( function (answer) {
+            connection.query("UPDATE employee SET ? WHERE ?", [
+                { role_id: answer.updateEmployee },
+                { id: answer.employeeID },
+            ]);
+            init();
+        });
+    });
+};
 
 
 init();
--- seeds.sql
+++ seeds.sql
@@ -7,24 +7,27 @@ VALUES
 ("HR"),
 ("Marketing"),
 ("Supply Chain"),
-("Finance");
+("Finance"),
+("Executive");
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-("Software Engineer", 85000, 1),
 ("Sr Software Engineer", 150000, 1),
+("Software Engineer", 85000, 1),
 ("Sales Rep", 45000, 2),
 ("HR Director", 120000, 3),
 ("PR Specialist", 70000, 4),
 ("Production Planner", 80000, 5),
-("Financial Advisor", 75000, 6);
+("Financial Advisor", 75000, 6),
+("Cheif Executive Officer", 250000, 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-("Bianca", "Mamome", 1, 1),
-("Braden", "Kim", 2, 7),
-("Silvia", "Lanchester", 3, 2),
-("Brandt", "Stark", 4, 7),
+("Bianca", "Mamome", 1, 8),
+("Braden", "Kim", 2, 1),
+("Silvia", "Lanchester", 3, 7),
+("Brandt", "Stark", 4, 8),
 ("Jess", "Snow", 5, 4),
-("Stephen", "Wade", 6, 5),
-("Steve", "Cangialosi", 7, 6);
+("Stephen", "Wade", 6, 8),
+("Steve", "Cangialosi", 7, 6),
+("Boss", "Captain", 8, NULL);
